8e01ab6019cfc92663c9e101503ee59fcd9f292f
Nishchey <37876680+Nishchey@users.noreply.github.com>
2019-03-14 11:16:40 +0530

    Renamed Variable

--- src/app/recipie/recipie.model.ts
+++ src/app/recipie/recipie.model.ts
@@ -1,13 +1,13 @@
 import { Ingredient } from './../Shared/ingredient.model';
 export class Recipie {
 public name: string;
-public discription: string;
+public description: string;
 public imagePath: string;
 public ingredients: Ingredient[];
 
 constructor(name: string, disc: string, imagePath: string,ingredients: Ingredient[]) {
 this.name =  name;
-this.discription = disc;
+this.description = disc;
 this.imagePath = imagePath;
 this.ingredients = ingredients;
    }
--- src/app/recipie/recipie.service.ts
+++ src/app/recipie/recipie.service.ts
@@ -12,12 +12,12 @@ export class RecipieService {
    recipies: Recipie[] = [
     // tslint:disable-next-line:max-line-length
     new Recipie('test recipie',
-    'test discription of recipie', 'https://pixnio.com/free-images/2017/09/21/2017-09-21-06-50-04-1100x825.jpg',
+    'test description of recipie', 'https://pixnio.com/free-images/2017/09/21/2017-09-21-06-50-04-1100x825.jpg',
     [
       new Ingredient('Lemon', 2),
       new Ingredient('Mint', 5)
     ]),
-    new Recipie('test 2 recipie', 'test 2 discription of recipie',
+    new Recipie('test 2 recipie', 'test 2 description of recipie',
      'http://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png',[
        new Ingredient('bun', 2),
        new Ingredient('potato patty', 1)
