7070809c1b70c8661449d810bf4357fd2b19b976
Nishchey <37876680+Nishchey@users.noreply.github.com>
2019-03-14 11:09:21 +0530

    Added Ingredients[] to Recipie Modal

--- src/app/recipie/recipie.model.ts
+++ src/app/recipie/recipie.model.ts
@@ -1,11 +1,14 @@
+import { Ingredient } from './../Shared/ingredient.model';
 export class Recipie {
 public name: string;
 public discription: string;
 public imagePath: string;
+public ingredients: Ingredient[];
 
-constructor(name: string, disc: string, imagePath: string) {
+constructor(name: string, disc: string, imagePath: string,ingredients: Ingredient[]) {
 this.name =  name;
 this.discription = disc;
 this.imagePath = imagePath;
+this.ingredients = ingredients;
    }
 }
--- src/app/recipie/recipie.service.ts
+++ src/app/recipie/recipie.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipie } from './recipie.model';
+import { Ingredient } from '../Shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,16 @@ export class RecipieService {
    recipies: Recipie[] = [
     // tslint:disable-next-line:max-line-length
     new Recipie('test recipie',
-    'test discription of recipie', 'https://pixnio.com/free-images/2017/09/21/2017-09-21-06-50-04-1100x825.jpg'),
+    'test discription of recipie', 'https://pixnio.com/free-images/2017/09/21/2017-09-21-06-50-04-1100x825.jpg',
+    [
+      new Ingredient('Lemon', 2),
+      new Ingredient('Mint', 5)
+    ]),
     new Recipie('test 2 recipie', 'test 2 discription of recipie',
-     'http://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png')];
+     'http://icons.iconarchive.com/icons/sonya/swarm/256/Fast-Food-icon.png',[
+       new Ingredient('bun', 2),
+       new Ingredient('potato patty', 1)
+     ])];
 
   constructor() { }
 
