0715c6ab0e96a24b0735ec0703fe10411fe2245c
Nishchey <37876680+Nishchey@users.noreply.github.com>
2019-03-26 10:44:27 +0530

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipieComponent } from './recipie/recipie.component';
-
+import { HeaderComponent } from './header/header.component';
 const routes: Routes = [
+  { path: '', redirectTo: '/app-header',  pathMatch: 'full' },
+  {path: 'app-recipie' , component: RecipieComponent},
   {path: 'ShoppingListComponent' , component: ShoppingListComponent},
   {path: 'RecipieComponent' , component: RecipieComponent}
 ];
@@ -12,4 +14,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponent = [ShoppingListComponent, RecipieComponent];
+export const routingComponent = [
+  ShoppingListComponent, RecipieComponent,HeaderComponent];
